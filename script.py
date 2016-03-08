49791cb67dce2a005c5471337f7ffb35ca243fad
Alex Diep <alexdiep2501@gmail.com>
2016-03-07 19:32:29 -0500

    added searching

--- script.py
+++ script.py
@@ -3,15 +3,15 @@ import os
 import re
 import pprint
 
-# Result is a nested dict
+# anno_dict is a nested dict
 # First key is the filename
 # Second key is the PeakID
 # Values is the rest of the fields
-result = {}
+anno_dict = {}
 
 for file_name in os.listdir():
     if file_name.endswith(".anno"):
-        result[file_name] = {}
+        anno_dict[file_name] = {}
 
         with open(file_name) as file_anno:
             # PeakID field has extra 'junk' so extra processing is needed
@@ -23,5 +23,34 @@ for file_name in os.listdir():
                 # Get value of PeakID and remove it from values
                 key = row.pop('PeakID')
                 # PeakID : { Other Values }
-                result[file_name][key] = row
+                anno_dict[file_name][key] = row
 
+# search_results is a nested dict
+# First key is the search word to find
+# Second key is the file name
+# Values is the number of hits
+search_results = {'non-coding':{},
+                  'intergenic':{},
+                  'intron':{},
+                  'exon':{},
+                  'promoter-TSS':{},
+                  'TTS':{},
+                  '5’ UTR':{},
+                  '3’ UTR':{}}
+
+for search_word in search_results.keys():
+    for file_name, peak_ids in anno_dict.items():
+        pattern = re.compile(search_word)
+        hits = 0
+
+        for fields in peak_ids.values():
+            # Combine all values so it is a 'row'
+            fields_str = ' '.join(fields.values())
+
+            if pattern.search(fields_str) is not None:
+                hits += 1
+
+        search_results[search_word][file_name] = hits
+
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(search_results)
