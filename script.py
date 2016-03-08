f1467c1cb8424ba080ff469c05549b728206ffe3
Alex Diep <alexdiep2501@gmail.com>
2016-03-07 19:05:03 -0500

    added comments

--- script.py
+++ script.py
@@ -1,14 +1,28 @@
 import csv
+import os
+import pprint
 
+# Result is a nested dict
+# First key is the filename
+# Second key is the PeakID
+# Values is the rest of the fields
 result = {}
-with open('test') as file:
-    fields = file.readline().split('\t')
-    fields[0] = 'PeakID'
-    
-    reader = csv.DictReader(file, fieldnames=fields, delimiter='\t')
-    for row in reader:
-        key = row.pop('PeakID')
-        result[key] = row
 
-print(result)
-    
+for file_name in os.listdir():
+    if file_name.endswith(".anno"):
+        result[file_name] = {}
+
+        with open(file_name) as file_anno:
+            # PeakID field has extra 'junk' so extra processing is needed
+            fields = file_anno.readline().split('\t')
+            fields[0] = 'PeakID'
+            
+            reader = csv.DictReader(file_anno, fieldnames=fields, delimiter='\t')
+            for row in reader:
+                # Get value of PeakID and remove it from values
+                key = row.pop('PeakID')
+                # PeakID : { Other Values }
+                result[file_name][key] = row
+
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(result['Botcheve_IMR90.anno'])
